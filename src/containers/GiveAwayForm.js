516658b204b76e5801fb2105a2bf57a55018f2c4
David Quiros <david@quiros.online>
2017-09-14 14:33:53 -0600

    Post item with image to share to trade network

--- src/containers/GiveAwayForm.js
+++ src/containers/GiveAwayForm.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import { gql, withApollo } from 'react-apollo'
+import Dropzone from 'react-dropzone'
 import { GC_USER_ID } from '../constants'
+
+import '../styles/GiveAwayForm.css';
 
 class GiveAwayForm extends Component {
 
@@ -12,7 +15,12 @@ class GiveAwayForm extends Component {
     itemTitle: '',
     itemPosted: false,
     itemTags: '',
+    itemImage: [],
     tags: new Map()
+  }
+
+  onDrop(imageFiles) {
+    this.setState({ itemImage: imageFiles })
   }
 
   async loadAllTags() {
@@ -24,59 +32,74 @@ class GiveAwayForm extends Component {
     this.setState({ tags: tags });
   }
 
-  async postFreeItem() {
-    const { itemTitle, itemTags, tags } = this.state;
+  postFreeItem() {
+    const { itemTitle, itemTags, itemImage, tags } = this.state;
     let itemTagsArray = itemTags.split(',');
     itemTagsArray = itemTagsArray.map(tag => tag.trim());
 
-    // Create new free item for user
-    const result = await this.props.client.mutate({
-      mutation: CREATE_ITEM_MUTATION,
-      variables: {
-        title: itemTitle,
-        ownerId: localStorage.getItem(GC_USER_ID),
-        status: 'FREE'
-      }
-    })
+    // prepare form data, use data key!
+    let data = new FormData()
+    data.append('data', itemImage[0])
 
-    const itemId = result.data.createItem.id;
+    // use the file endpoint
+    fetch('https://api.graph.cool/file/v1/cj7gdhdwb02te01141lbxk8vo', {
+      method: 'POST',
+      body: data
+    }).then(response => {
+      return response.json()
+    }).then(image => {
 
-    // Show confirmation message
-    this.setState({
-      itemTitle: '',
-      itemTags: '',
-      itemPosted: true
-    });
+      // Create new free item for user
+      this.props.client.mutate({
+        mutation: CREATE_ITEM_MUTATION,
+        variables: {
+          title: itemTitle,
+          ownerId: localStorage.getItem(GC_USER_ID),
+          status: 'FREE',
+          imageId: image.id
+        }
+      }).then(result => {
+        const itemId = result.data.createItem.id;
 
-    setTimeout(() => {
-      this.setState({
-        itemPosted: false
-      });
-    }, 3000);
-
-    const tagsPromises = itemTagsArray.map(tag => {
-      if (tags.has(tag)) {
-        // tag already exists; return a resolved promise
-        return Promise.resolve(true);
-      } else {
-        return this.props.client.mutate({
-          mutation: CREATE_TAG_MUTATION,
-          variables: {
-            tag: tag,
-            itemsIds: [ itemId ],
-          }
+        // Show confirmation message
+        this.setState({
+          itemTitle: '',
+          itemTags: '',
+          itemImage: [],
+          itemPosted: true
         });
-      }
-    })
 
-    Promise.all(tagsPromises).then((values) => {
-      console.log('Tags were resolved: ', values);
-    });
+        setTimeout(() => {
+          this.setState({
+            itemPosted: false
+          });
+        }, 3000);
+
+        const tagsPromises = itemTagsArray.map(tag => {
+          if (tags.has(tag)) {
+            // tag already exists; return a resolved promise
+            return Promise.resolve(true);
+          } else {
+            return this.props.client.mutate({
+              mutation: CREATE_TAG_MUTATION,
+              variables: {
+                tag: tag,
+                itemsIds: [ itemId ],
+              }
+            });
+          }
+        })
+
+        Promise.all(tagsPromises).then((values) => {
+          console.log('Tags were resolved: ', values);
+        });
+      })
+    })
   }
 
   render() {
 
-    const { itemPosted } = this.state;
+    const { itemPosted, itemImage } = this.state;
 
     return (
       <div>
@@ -94,6 +117,18 @@ class GiveAwayForm extends Component {
             type='text'
             placeholder="Item Tags"
           />
+
+          <Dropzone onDrop={ this.onDrop.bind(this) }
+                    accept='image/*'
+                    className='dropzone'
+                    activeClassName='active-dropzone'
+                    multiple={false}>
+            {itemImage.length === 0 &&
+              <div className="hint">Drag and drop or click to select an image of your gift</div> }
+            {itemImage.length > 0 &&
+              <img src={itemImage[0].preview} alt={this.state.itemTitle} /> }
+          </Dropzone>
+
           <button onClick={ (e) => {
             e.preventDefault();
             this.postFreeItem()
@@ -129,11 +164,12 @@ const CREATE_TAG_MUTATION = gql`
 `
 
 const CREATE_ITEM_MUTATION = gql`
-  mutation CreateItemMutation ($title: String!, $ownerId: ID!, $status: ItemStatus!) {
+  mutation CreateItemMutation ($title: String!, $ownerId: ID!, $status: ItemStatus!, $imageId: ID!) {
     createItem(
       title: $title,
       ownerId: $ownerId,
-      status: $status
+      status: $status,
+      imageId: $imageId
     ) {
       id
     }
